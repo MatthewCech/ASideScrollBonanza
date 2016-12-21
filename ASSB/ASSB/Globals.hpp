6746fbfcacadddee741233f7aac997c7f020bb51
GHOST\rever <reveriewisp@gmail.com>
2016-12-21 14:00:26 -0800

    + Added globals header

---
+++ ASSB/ASSB/Globals.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "EventSystem\EventSystem.hpp"
+
+
+namespace ASSB
+{
+	// Global Variables
+	static EventSystem::EventSystem EventSystemInstance = EventSystem::EventSystem();
+}
