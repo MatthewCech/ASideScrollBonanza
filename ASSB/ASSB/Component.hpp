fb81e19362837f572e00595ecfb0f12d657e3d15
GHOST\rever <reveriewisp@gmail.com>
2016-12-21 14:09:26 -0800

    + added component base

--- ASSB/ASSB/Component.hpp
+++ ASSB/ASSB/Component.hpp
@@ -1,1 +1,18 @@
 #pragma once
+#include "EventSystem\ObjectEventManager.hpp"
+
+
+
+namespace ASSB
+{
+	// A base-class to allow us to standardize component contents when necessary.
+	class Component : public EventSystem::ObjectEventManager
+	{
+	public:
+		// Constructor / Destructor
+		Component();
+		virtual ~Component();
+	private:
+		Component &operator=(const Component &rhs) = delete;
+	};
+}
